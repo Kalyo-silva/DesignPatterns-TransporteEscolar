eba7c88ea3d30e05ffcb7b3040c5109f97ed809e
kalyo-silva <kalyo.silva@unidavi.edu.br>
2025-11-27 11:10:23 -0300

    ajustes

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from domain.Rotas import Rota
-from domain.Veiculos import Veiculo
-from strategies.CobrancaStrategy import *
-from decorators.RouteExecutor import RouteExecutor
 import app.interface as interface
 
 interface.mainLogo()
 interface.mainMenu()
-
-#print(executor.ExecutaR
